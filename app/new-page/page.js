bb246c5cf49bc540a85bb268b8834058ed9edfb8
shoaib.muhammad <shoaib.muhammad@launchboxglobal.co>
2026-06-05 21:00:07 +0500

    creating landing page

---
+++ app/new-page/page.js
@@ -0,0 +1,13 @@
+import NewPage from "@/components/NewPage/NewPage";
+import React from "react";
+
+export const metadata = {
+  title: "New Home Page",
+  description: "New Home Page",
+};
+
+const page = () => {
+  return <NewPage />;
+};
+
+export default page;
